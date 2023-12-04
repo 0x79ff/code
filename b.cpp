246fa867acbdae44fb3ed79cf620354e3ceb7d37
0x79ff <2295860791@qq.com>
2023-12-04 16:52:03 +0800

    t

--- b.cpp
+++ b.cpp
@@ -3,22 +3,44 @@ using namespace std;
 typedef long long ll;
 #define endl '\n'
 
-void solve()
-{
-    
+const int N = 2e5+10;
+ll l[N],r[N];
+ll now[N];
+
+struct node{
+    ll x,y;
+};
+unordered_map<node,int> vis;
+
+vector<node> a;
+
+bool cmp(node t1,node t2){
+    return t1.y < t2.y;
 }
+
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    int T=1;
-    cin>>T;
-    while(T--){
-        solve();
+    // ios::sync_with_stdio(false);
+    // cin.tie(0);
+    // cout.tie(0);
+    int n,m;
+    for(int i=1;i<=m;i++){
+        ll l,r;cin>>l>>r;
+        a.push_back({i,l});
+        a.push_back({i,r});
     }
     
+    sort(a.begin(),a.end(),cmp);
+
+    queue<ll> c;
+
+    for(int i=0;i<a.size();i++){
+        if(now[a[i].x]==0){
+            c.push_back();
+        }
+    }
+
 
     return 0;
 }
