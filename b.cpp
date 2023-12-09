3895c397d87db277dc7f6fe564a24c6a85e4e5f0
0x79ff <2295860791@qq.com>
2023-12-09 12:02:57 +0800

    " "

--- b.cpp
+++ b.cpp
@@ -3,74 +3,66 @@ using namespace std;
 typedef long long ll;
 #define endl '\n'
 
-const int N = 1e4+10;
-const int D = 30;
-
-ll w[N],q[N][D],k[N][D],v[N][D],kt[D][N];
-ll b[D][D],c[N][D];
 
 int main()
 {
     // ios::sync_with_stdio(false);
     // cin.tie(0);
     // cout.tie(0);
-    
-    int n,d;
-    cin>>n>>d;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=d;j++){
-            cin>>q[i][j];
-        }
-    }
-    
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=d;j++){
-            cin>>k[i][j];
-            kt[j][i]=k[i][j];
-        }
-    }
-    
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=d;j++){
-            cin>>v[i][j];
-        }
-    }   
-
-    for(int i=1;i<=n;i++){
-        cin>>w[i];
-    }
-
-
-    for(int i=1;i<=d;i++){
-        for(int j=1;j<=d;j++){
-            for(int x=1;x<=n;x++){
-                b[i][j]+=kt[i][x]*v[x][j];
+    int n;
+    cin>>n;
+    unordered_map<int,array<int,2>> b;//place begintime endtime
+    unordered_map<int,bool> vis;//is visited  !!why
+    set<array<int,3>> c;//time person place 
+    for(int day=0;day<n;day++){
+        int r,m;
+        cin>>r>>m;
+        while(r--){
+            int p;
+            cin>>p;
+            if(!vis[p]){
+                vis[p]=1;
+                b[p][0]=day;
+                b[p][1]=day+6;
+            }
+            else{   
+                if(b[p][1]+1>=day){
+                    b[p][1]=day+6;
+                }
+                else{
+                    b[p][0]=day;
+                    b[p][1]=day+6;
+                }   
             }
         }
-    }
+        while(m--){
+            array<int,3> t;
+            cin>>t[0]>>t[1]>>t[2];
+            c.insert(t);
+        }
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=d;j++){
-            for(int x=1;x<=d;x++){
-                c[i][j]+=q[i][x]*b[x][j];
+        set<int> ans;
+        set<array<int,3>> newc;
+        for(auto s:c){
+            if(s[0]+6<day){
+                ;
+            }
+            else{
+                newc.insert(s);
+                if(b[s[2]][0] <= s[0] && b[s[2]][1]>=day && vis[s[2]]){
+                    ans.insert(s[1]);
+                }
             }
         }
-    }
-    
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=d;j++){
-            c[i][j]*=w[i];
-        }
-    }
+        c=newc;
 
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=d;j++){
-            cout<<c[i][j]<<" ";
+        cout<<day<<" ";
+        for(auto s:ans){
+            cout<<s<<" ";
         }
         cout<<endl;
+        
     }
-    
-
     return 0;
 }
 
---
+++ t.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define endl '\n'
+
+
+int main()
+{
+    // ios::sync_with_stdio(false);
+    // cin.tie(0);
+    // cout.tie(0);
+    set<int> a;
+    a.insert(2);
+    
+    a.insert(1);
+    for(auto s:a){
+        cout<<s<<" ";
+    }
+    return 0;
+}
+
